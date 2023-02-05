c9d56044acf3968bfdf5ea425c551fb49d79923f
ramo828 <illegalism666@gmail.com>
2023-02-06 01:12:28 +0400

    fix log

--- bot.py
+++ bot.py
@@ -1,16 +1,9 @@
 import time
 import telepot
 from command import Command
-from selenium import webdriver
-from selenium.webdriver.common.keys import Keys
-from selenium.webdriver.chrome.options import Options
-from selenium.webdriver.common.by import By
-from selenium.webdriver.support import expected_conditions as EC
-from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.common.action_chains import ActionChains
 from time import sleep
 
-bot1 = telepot.Bot('---')
+bot1 = telepot.Bot('----')
 def handle(msg):
     content_type = telepot.glance(msg)[0]
     chat_type = telepot.glance(msg)[1]
