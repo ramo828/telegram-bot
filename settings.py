d25750c2db8476fd3b0235edc561197581789745
ramo828 <illegalism666@gmail.com>
2023-02-08 02:35:17 +0400

    readme edit

--- settings.py
+++ settings.py
@@ -2,9 +2,9 @@
     Developper: Ramiz Mammadli
 """
 
-telegram_api = "6143344781:AAGOKuVu2PBjyqIZTh8HT2j4Ki9gGjYZjOc"
-duckdns_api = "3ccf5a5e-ead7-4066-b378-11729426772e"
-duckdns_domain_name = "opiaz"
+telegram_api = ""
+duckdns_api = ""
+duckdns_domain_name = ""
 modem_login = "admin"
 modem_pass = "admin"
 modem_url = "192.168.1.1"
